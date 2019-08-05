60b4cb1516090d5c518cd5791cfd33374d11dabd
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2019-08-05 14:00:21 +0300

    Split data

--- chars74k/preprocessing/utils.py
+++ chars74k/preprocessing/utils.py
@@ -1,21 +1,25 @@
 from tqdm import tqdm
+from shutil import copyfile
 import os
 import cv2
 import matplotlib.pyplot as plt
 from random import shuffle
 
+CONST_TRAIN = 'Train'
+CONST_VAL = 'Val'
+CONST_TEST = 'Test'
+CONST_ALL = 'All'
 
 def load_data(folder_path, imsize):
     data = []
     for class_folder in tqdm(os.listdir(folder_path)):
         class_folder_path = os.path.join(folder_path, class_folder)
         label = int(class_folder.split("Sample")[1])
-        for img in tqdm(os.listdir(class_folder_path)):
+        for img in os.listdir(class_folder_path):
             image_path = os.path.join(class_folder_path, img)
             img = cv2.imread(image_path, cv2.IMREAD_COLOR)
             img = cv2.resize(img, (imsize, imsize))
             data.append([img, label])
-    print('Loaded successfully')
     return data
 
 
@@ -35,3 +39,37 @@ def plot_random_samples(labeled_images, row_size, col_size):
         y.get_xaxis().set_visible(False)
         y.get_yaxis().set_visible(False)
     plt.show()
+
+
+def copy_imgs(src, dst, img_names):
+    for img_name in img_names:
+        src_path = os.path.join(src, img_name)
+        dst_path = os.path.join(dst, img_name)
+        copyfile(src_path, dst_path)
+
+
+def preprocess_images(folder_path):
+    class_folders = os.listdir(os.path.join(folder_path, CONST_ALL))
+    type_folders = [CONST_TEST, CONST_TRAIN, CONST_VAL]
+
+    for type_folder in type_folders:
+        if os.path.isdir(os.path.join(folder_path, type_folder)):
+            print ('Can not process.\n' + '"' + type_folder + '" already exist.')
+            return
+
+        for class_folder in class_folders:
+            os.makedirs(os.path.join(folder_path, type_folder, class_folder))
+
+    for class_folder in tqdm(class_folders):
+        imgs = os.listdir(os.path.join(folder_path, CONST_ALL, class_folder))
+        shuffle(imgs)
+
+        train_imgs = imgs[0 : int(len(imgs) * 0.7)]
+        val_imgs = imgs[int(len(imgs) * 0.7) : int(len(imgs) * 0.8)]
+        test_imgs = imgs[int(len(imgs) * 0.8) : len(imgs)]
+
+        src_path = os.path.join(folder_path, CONST_ALL, class_folder)
+
+        copy_imgs(src_path, os.path.join(folder_path, CONST_TRAIN, class_folder), train_imgs)
+        copy_imgs(src_path, os.path.join(folder_path, CONST_VAL, class_folder), val_imgs)
+        copy_imgs(src_path, os.path.join(folder_path, CONST_TEST, class_folder), test_imgs)
